35f28dbda5ad85e4005ad3e04a45a51cba10b874
garyjo <gary.uppiah@icloud.com>
2017-02-06 15:47:08 -0500

    Modification

--- 2.Patterns/300098957/src/Singleton/SingletonDemo.java
+++
@@ -1,9 +0,0 @@
-// File Name: SingletonDemo.java
-public class SingletonDemo {
-
-    public static void main(String[] args) {
-        Singleton tmp = Singleton.getInstance( );
-        tmp.demoMethod( );
-
-    }
-}
